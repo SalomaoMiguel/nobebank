c0011dd80c6e4f64783620ed5be06a8b9b924bb8
salomaoMiguel <salomaombono@outlook.com>
2021-02-15 18:51:32 -0300

    NobeBank -> Bug - erro de impleemntacao de valor nao pode ficar zerado

--- app/models/agencium.rb
+++ app/models/agencium.rb
@@ -4,7 +4,7 @@ class Agencium < ApplicationRecord
   belongs_to :banco
   has_many :contums
   #Validacao
-  validates :descricao, presence: true, length: {maximum: 255}
+  validates :descricao, presence: true, length: {maximum: 250}
   validates :codigo, presence: true, length: {maximum: 255}, uniqueness: true
   validates :numero, presence: true, length: {maximum: 255}
   validates :ativo, presence: true, inclusion: {in: [true, false]}
